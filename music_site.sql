34c32ccc16095e23c4ba1c49eef3819e8e5d793e
Jakub Wiczyński <144121966+kabi106@users.noreply.github.com>
2024-04-13 14:18:48 +0200

    Add files via upload

--- music_site.sql
+++ music_site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2024 at 11:42 AM
+-- Generation Time: Apr 13, 2024 at 02:17 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -31,7 +31,7 @@ CREATE TABLE `playlists` (
   `playlist_id` int(11) NOT NULL,
   `playlist_name` varchar(255) NOT NULL,
   `numOfSongs` int(11) NOT NULL,
-  `createdBy` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
   `playlistType` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,11 +39,14 @@ CREATE TABLE `playlists` (
 -- Dumping data for table `playlists`
 --
 
-INSERT INTO `playlists` (`playlist_id`, `playlist_name`, `numOfSongs`, `createdBy`, `playlistType`) VALUES
-(83, 'bruh', 0, '', 'Local'),
-(84, 'bruh2', 0, '', 'Local'),
-(85, 'chuj', 0, '', 'Public'),
-(86, 'dfghj', 0, '', 'Local');
+INSERT INTO `playlists` (`playlist_id`, `playlist_name`, `numOfSongs`, `user`, `playlistType`) VALUES
+(91, 'new', 0, 'new', 'Local'),
+(92, 'new', 0, 'new', 'Local'),
+(93, 'new', 0, 'new', 'Local'),
+(94, 'test', 0, 'tester', 'Private'),
+(95, 'public', 0, 'new', 'Public'),
+(96, 'x', 0, 'new', 'Local'),
+(97, 'k', 0, 'new', 'Private');
 
 -- --------------------------------------------------------
 
@@ -61,10 +64,8 @@ CREATE TABLE `playlist_songs` (
 --
 
 INSERT INTO `playlist_songs` (`playlist_id`, `song_id`) VALUES
-(83, 111),
-(83, 112),
-(83, 114),
-(84, 113);
+(95, 130),
+(96, 131);
 
 -- --------------------------------------------------------
 
@@ -75,7 +76,7 @@ INSERT INTO `playlist_songs` (`playlist_id`, `song_id`) VALUES
 CREATE TABLE `songs` (
   `song_id` int(11) NOT NULL,
   `song_name` varchar(255) NOT NULL,
-  `artist` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
   `length` int(11) NOT NULL,
   `audio_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -84,11 +85,48 @@ CREATE TABLE `songs` (
 -- Dumping data for table `songs`
 --
 
-INSERT INTO `songs` (`song_id`, `song_name`, `artist`, `length`, `audio_path`) VALUES
-(111, 'example1', '', 0, './audio/gitara_bit.mp3'),
-(112, 'xd', '', 0, './audio/gitara_bit.mp3'),
-(113, 'ghj', '', 0, './audio/gitara_bit.mp3'),
-(114, 'qwertyuio', '', 0, './audio/gitara_bit.mp3');
+INSERT INTO `songs` (`song_id`, `song_name`, `user`, `length`, `audio_path`) VALUES
+(129, 'xd', 'new', 0, './audio/gitara_bit.mp3'),
+(130, '[value-2]', '[value-3]', 0, '[value-5]'),
+(131, 'fcsd', 'new', 0, './audio/gitara_bit.mp3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
+(1, 'dcs', '$2y$10$tJk449AJC.mVPAXvsVIY8uJ6o4kQd4pRH/2jdEAkC0fLtPjlnmvw.', 'sdcdc@rdfvg.gf'),
+(2, 'dcse', '$2y$10$0xobxcJdbBH0C6c.jeHBl.RPTJYJNM5.MD4h5YCXNEFo9wzpAHQyO', 'sdcddc@rdfvg.gf'),
+(3, 'dx', '$2y$10$VHo6vOcabYjjGFRezoV2Yuuo8d9jhAU87bun1ZUs2cx3Jm3aESrgu', 'cds@cds.cd'),
+(4, 'dfgfdg', '$2y$10$jH6Z3X7ldwVKr5vBQAM6GOiTqL.dvIIMO74dGGfAnT5XYys8upHO6', 'sfdunfds@nuvf.pl'),
+(5, 'dcdsc', '$2y$10$b7vVpWgc8E2qq9eJDbOfeeBeT9nO9WsZnx22a/2EAlc5HLY33E7WK', 'dsc@dsc.cd'),
+(6, 'ko;scd', '$2y$10$4G4FRcaSi0lmJsSNDL3xruOJpaHdrO13R.01llPz/q1m1rCtaeRYi', 'dcnuk@jnk.pl'),
+(7, 'new', '$2y$10$6Y9llJfbEf7ckoP7TCsXDOBrylcKiv2QdPSIdsLoTEFlpLpNFnDa.', 'new@new.pl');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user_likes`
+--
+
+CREATE TABLE `user_likes` (
+  `like_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indeksy dla zrzutów tabel
@@ -114,6 +152,22 @@ ALTER TABLE `songs`
   ADD PRIMARY KEY (`song_id`);
 
 --
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeksy dla tabeli `user_likes`
+--
+ALTER TABLE `user_likes`
+  ADD PRIMARY KEY (`like_id`),
+  ADD UNIQUE KEY `user_song_unique` (`user_id`,`song_id`),
+  ADD KEY `fk_user_likes_song_id` (`song_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -121,13 +175,25 @@ ALTER TABLE `songs`
 -- AUTO_INCREMENT for table `playlists`
 --
 ALTER TABLE `playlists`
-  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_likes`
+--
+ALTER TABLE `user_likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -139,9 +205,15 @@ ALTER TABLE `songs`
 ALTER TABLE `playlist_songs`
   ADD CONSTRAINT `playlist_songs_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`),
   ADD CONSTRAINT `playlist_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`);
+
+--
+-- Constraints for table `user_likes`
+--
+ALTER TABLE `user_likes`
+  ADD CONSTRAINT `fk_user_likes_song_id` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_likes_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
